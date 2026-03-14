982ae6a4144e05c65d5991a5fe998583beae2a83
Anyela Martinez <anyela.martinez@udea.edu.co>
2026-03-14 00:27:02 -0500

    Creación del archivo .cpp para desarrollo

---
+++ Primer_Desafio_Tetris/funciones_tetris.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
+
+void crear_Tablero(){
+    //Aqui va la creación del tablero con las dimensiones
+};
+
+void generarFichas() {
+    //Se generan las fichas aleatoriamente
+};
+
+void rotar_Fichas() {
+    //Rotar a la derecha o izquierda las fichas
+};
+
+void mover_Derecha() {
+    //Mover bits a la derecha (aun no sé si en la misma función mover a izq y a der o hacerlas separadas)
+};
+
+void mover_Izquierda() {
+    //Mover bits a la izquierda
+};
+
+void verificar_Colisione() {
+    //Verificar si la ficha se choca con algo para parar o seguir bajando
+};
+
+void eliminar_Filas() {
+    //Eliminar las filas que esten completas con 1
+};
---
+++ Primer_Desafio_Tetris/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Aqui va el main";
+    return 0;
+    //ejecutar el juego con todas las funciones, creo que lo completaria al final
+}
